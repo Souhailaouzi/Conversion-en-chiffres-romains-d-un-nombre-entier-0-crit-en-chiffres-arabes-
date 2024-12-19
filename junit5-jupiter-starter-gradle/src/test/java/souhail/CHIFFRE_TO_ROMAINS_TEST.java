b310df7170c1316155d32219c3f0e4f0a072c5ec
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:12:14 +0100

    Red : Test pour convertir 1 en 'I'

---
+++ junit5-jupiter-starter-gradle/src/test/java/souhail/CHIFFRE_TO_ROMAINS_TEST.java
@@ -0,0 +1,13 @@
+package souhail;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CHIFFRE_TO_ROMAINS_TEST{
+
+    @Test
+    public void testConvert1() {
+            assertEquals("I", convert(1));  // Test pour 1
+        }
+
+    }
+    
