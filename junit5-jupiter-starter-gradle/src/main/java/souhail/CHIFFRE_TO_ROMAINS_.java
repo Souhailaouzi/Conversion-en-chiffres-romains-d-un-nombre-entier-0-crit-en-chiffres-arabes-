0ee4425fc1ac7df35b7478695c46cb1901fb0c38
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:16:36 +0100

    REFACTOR : amelioration afin de traiter tout les cas possibles

--- junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
@@ -1,17 +1,22 @@
 package souhail;
 public class CHIFFRE_TO_ROMAINS_ {
-    public static String convert(int nbr) {
-        if (nbr == 1) {
-            return "I";  // Cas pour 1
+    public static String convert(int nbr){
+        String[] romains = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] chiffre = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        String res = "";
+        
+        for(int i=0;i<chiffre.length;i++){
+            while(nbr>=chiffre[i]){
+                res = res + romains[i];
+                nbr = nbr - chiffre[i];
+
+            }
         }
-        if (nbr == 5) {
-            return "V";  // Cas pour 5
-        }
-        if (nbr == 10) {
-            return "X";  // Cas pour 10
-        }
-        return "";
-    }
+        return res;
+
+}
+
 }
 
 
