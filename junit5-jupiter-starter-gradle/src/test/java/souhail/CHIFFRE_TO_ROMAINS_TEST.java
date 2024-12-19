9d3cbaf415c7c75210ec136b370bc3cf055b836b
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:13:55 +0100

    Red : Test pour convertir 5 en 'V'

--- junit5-jupiter-starter-gradle/src/test/java/souhail/CHIFFRE_TO_ROMAINS_TEST.java
+++ junit5-jupiter-starter-gradle/src/test/java/souhail/CHIFFRE_TO_ROMAINS_TEST.java
@@ -4,10 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CHIFFRE_TO_ROMAINS_TEST{
 
-    @Test
-    public void testConvert1() {
+@Test
+public void testConvert1() {
             assertEquals("I", CHIFFRE_TO_ROMAINS_.convert(1));  // Test pour 1
         }
+
+@Test
+void testConvert5() {
+    assertEquals("V", convert(5));  // Test pour 5
+}
+
 
     }
     
