a3ce3a4a5cc669fc72bf7f0ea024d4246a331a13
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:15:09 +0100

    Red : Test pour convertir 10 en 'X'

--- junit5-jupiter-starter-gradle/src/test/java/souhail/CHIFFRE_TO_ROMAINS_TEST.java
+++ junit5-jupiter-starter-gradle/src/test/java/souhail/CHIFFRE_TO_ROMAINS_TEST.java
@@ -6,14 +6,18 @@ public class CHIFFRE_TO_ROMAINS_TEST{
 
 @Test
 public void testConvert1() {
-            assertEquals("I", CHIFFRE_TO_ROMAINS_.convert(1));  // Test pour 1
+            assertEquals("I", CHIFFRE_TO_ROMAINS_.convert(1));  
         }
 
 @Test
 void testConvert5() {
-    assertEquals("V", convert(5));  // Test pour 5
+    assertEquals("V", CHIFFRE_TO_ROMAINS_.convert(5));
 }
 
+@Test
+void testConvert10() {
+    assertEquals("X", CHIFFRE_TO_ROMAINS_.convert(10)); 
+}
 
     }
     
