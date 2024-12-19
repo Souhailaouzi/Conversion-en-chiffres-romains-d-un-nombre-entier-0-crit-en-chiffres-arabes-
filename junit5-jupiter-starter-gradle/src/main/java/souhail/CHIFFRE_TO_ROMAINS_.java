f052eaaa71e213cbee5ea874e3f5ee9f27562fe9
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:15:30 +0100

    Green : Conversion de 10 en 'X'

--- junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
@@ -2,10 +2,13 @@ package souhail;
 public class CHIFFRE_TO_ROMAINS_ {
     public static String convert(int nbr) {
         if (nbr == 1) {
-            return "I"; 
+            return "I";  // Cas pour 1
         }
         if (nbr == 5) {
-            return "V";  
+            return "V";  // Cas pour 5
+        }
+        if (nbr == 10) {
+            return "X";  // Cas pour 10
         }
         return "";
     }
