df80120a1f51369a7fd3ebb3c561e028a37908e9
SOUHAIL AOUZI <souhailaouzi1949@gmail.com>
2024-12-19 17:14:22 +0100

    Green : Conversion de 5 en 'V'

--- junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
+++ junit5-jupiter-starter-gradle/src/main/java/souhail/CHIFFRE_TO_ROMAINS_.java
@@ -2,7 +2,10 @@ package souhail;
 public class CHIFFRE_TO_ROMAINS_ {
     public static String convert(int nbr) {
         if (nbr == 1) {
-            return "I";  // Cas simple
+            return "I"; 
+        }
+        if (nbr == 5) {
+            return "V";  
         }
         return "";
     }
